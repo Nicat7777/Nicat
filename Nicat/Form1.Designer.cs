60bba67a8133df8905151b7cc81d67e7e7641574
Nicat <Nicat@WIN-RVTIE21EIN8>
2025-09-25 23:15:19 +0400

    Add project files.

---
+++ Nicat/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Nicat
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Green;
+            label1.Location = new Point(222, 121);
+            label1.Name = "label1";
+            label1.Size = new Size(51, 21);
+            label1.TabIndex = 0;
+            label1.Text = "Gmail";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.Green;
+            label2.Location = new Point(222, 194);
+            label2.Name = "label2";
+            label2.Size = new Size(76, 21);
+            label2.TabIndex = 1;
+            label2.Text = "Password";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(292, 119);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "user@gmail.com";
+            textBox1.Size = new Size(198, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(304, 192);
+            textBox2.Name = "textBox2";
+            textBox2.PlaceholderText = "********";
+            textBox2.Size = new Size(186, 23);
+            textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.CornflowerBlue;
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.ForeColor = SystemColors.ButtonHighlight;
+            button1.Location = new Point(321, 265);
+            button1.Name = "button1";
+            button1.Size = new Size(110, 36);
+            button1.TabIndex = 4;
+            button1.Text = "Sign in";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LemonChiffon;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+    }
+}
---
+++ Nicat/Form1.cs
@@ -0,0 +1,76 @@
+﻿namespace Nicat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text.Length < 8)
+            {
+                MessageBox.Show("Şifrə ən az 8 simvoldan ibarət olmalıdır");
+                return;
+            }
+
+            string xususiSimvol = "!@#$%^&*()-_=+[]{}|;:'\",.<>?/`~";
+            string boyukHerfler = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string kicikHerfler = "abcdefghijklmnopqrstuvwxyz";
+            string reqemler = "0123456789";
+
+            bool boyukVar = false;
+            bool kicikVar = false;
+            bool reqemVar = false;
+            bool xususiVar = false;
+            bool ardicil3EyniSimvol = false;
+
+            char evvelkiSimvol = '\0';
+            int say = 1;
+
+            for (int i = 0; i < textBox2.Text.Length; i++)
+            {
+                char simvol = textBox2.Text[i];
+
+                if (reqemler.Contains(simvol)) reqemVar = true;
+                else if (boyukHerfler.Contains(simvol)) boyukVar = true;
+                else if (kicikHerfler.Contains(simvol)) kicikVar = true;
+                else if (xususiSimvol.Contains(simvol)) xususiVar = true;
+
+                if (simvol == evvelkiSimvol)
+                {
+                    say++;
+                    if (say >= 3)
+                        ardicil3EyniSimvol = true;
+                }
+                else
+                {
+                    say = 1;
+                }
+
+                evvelkiSimvol = simvol;
+            }
+
+            if (reqemVar && kicikVar && boyukVar && xususiVar && !ardicil3EyniSimvol)
+            {
+                MessageBox.Show("Şifrə uğurla yaradıldı");
+            }
+            else
+            {
+                if (!boyukVar) MessageBox.Show("Ən az 1 böyük hərf olmalıdır");
+                if (!kicikVar) MessageBox.Show("Ən az 1 kiçik hərf olmalıdır");
+                if (!reqemVar) MessageBox.Show("Ən az 1 rəqəm olmalıdır");
+                if (!xususiVar) MessageBox.Show("Ən az 1 xüsusi simvol olmalıdır");
+                if (ardicil3EyniSimvol) MessageBox.Show("Eyni simvol ard-arda 3 dəfə gələ bilməz");
+            }
+
+
+        }
+    }
+}
